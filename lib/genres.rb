bca85f6472df17e3e187079a70f7e5294abd21fe
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-01-31 00:07:57 +0000

    Done.

---
+++ lib/genres.rb
@@ -0,0 +1,23 @@
+class Genres
+attr_accessor :name, :songs
+@@all []
+
+def self.all # class reader
+  @@all
+end
+
+def initialize
+  save
+end
+
+def save
+  @@all << self
+end
+
+def self.reset_all
+  @@all.clear
+end
+
+def self.count
+  @@all.size
+end
