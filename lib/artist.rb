05a71ae4100f4a32f0496ffc1d5714fd184e205d
Latricia Nickelberry <tricianickelberry@gmail.com>
2020-01-31 00:15:55 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,23 +1,43 @@
 class Artist
-attr_accessor :name, :songs
-@@all []
+  extend Concerns::Findable
 
-def self.all # class reader
-  @@all
-end
+  attr_accessor :name
+  attr_reader :songs
 
-def initialize
-  save
-end
+  @@all = []
 
-def save
-  @@all << self
-end
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-def self.reset_all
-  @@all.clear
-end
+  def self.all
+    @@all
+  end
 
-def self.count
-  @@all.size
+  def self.destroy_all
+    all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    artist = new(name)
+    artist.save
+    artist
+
+    # Or, as a one-liner:
+    # new(name).tap{ |a| a.save }
+  end
+
+  def add_song(song)
+    song.artist = self unless song.artist
+    songs << song unless songs.include?(song)
+  end
+
+  def genres
+    songs.collect{ |s| s.genre }.uniq
+  end
 end
--- lib/genres.rb
+++ lib/genres.rb
@@ -1,23 +1,38 @@
-class Genres
-attr_accessor :name, :songs
-@@all []
+class Genre
+  extend Concerns::Findable
 
-def self.all # class reader
-  @@all
-end
+  attr_accessor :name
+  attr_reader :songs
 
-def initialize
-  save
-end
+  @@all = []
 
-def save
-  @@all << self
-end
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
 
-def self.reset_all
-  @@all.clear
-end
+  def self.all
+    @@all
+  end
 
-def self.count
-  @@all.size
+  def self.destroy_all
+    all.clear
+  end
+
+  def save
+    self.class.all << self
+  end
+
+  def self.create(name)
+    genre = new(name)
+    genre.save
+    genre
+
+    # Or, as a one-liner:
+    # new(name).tap{ |g| g.save }
+  end
+
+  def artists
+    songs.collect{ |s| s.artist }.uniq
+  end
 end
